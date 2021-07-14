b1ff8fcc8b8c5523874744f4ebe471e0630db9cc
myller-silva <myllersilva1310@gmail.com>
2021-07-14 12:24:22 -0300

    Create Matriz.java

---
+++ Matrizes/Matriz.java
@@ -0,0 +1,10 @@
+public class Matriz {
+    public static void main(String[] args) {
+        // DECLARAÇÃO:
+        int[][] n1 = {{0,0,0}, {0,0,0}, {0,0,0}};
+        // ou
+        final int lin=3;
+        final int col=3;
+        int[][] n2 = new int[lin][col];
+    }
+}

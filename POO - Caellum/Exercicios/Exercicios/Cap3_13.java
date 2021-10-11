6a37432bc1027ace737d487ea8368f0e93a2b2d3
myller-silva <myllersilva1310@gmail.com>
2021-10-11 19:31:46 -0300

    update

--- POO - Caellum/Exercicios/Exercicios/Cap3_13.java
+++ POO - Caellum/Exercicios/Exercicios/Cap3_13.java
@@ -20,24 +20,57 @@ public class Cap3_13 {
         //     }     
         // }
 
-        4
-        for(int c=1; c<10; c++){
-            int fatorial=1;
-            System.out.printf("\n%d! = ", c);
-            for(int i=c; i>=1; i--){
-                fatorial *= i;
-            }
-            for(int i=c; i>=1; i--){
-                System.out.printf("%d", i);
-                if(i>1){
-                    System.out.printf(" x ");
-                }else{
-                    System.out.printf(" = ");
-                }
-            }
-            System.out.println(fatorial);
-        }
+        // 4
+        // for(int c=1; c<10; c++){
+        //     int fatorial=1;
+        //     System.out.printf("\n%d! = ", c);
+        //     for(int i=c; i>=1; i--){
+        //         fatorial *= i;
+        //     }
+        //     for(int i=c; i>=1; i--){
+        //         System.out.printf("%d", i);
+        //         if(i>1){
+        //             System.out.printf(" x ");
+        //         }else{
+        //             System.out.printf(" = ");
+        //         }
+        //     }
+        //     System.out.println(fatorial);
+        // }
 
+        // 5
+        // for(int c=1; c<40; c++){
+        //     long fatorial = 1;
+        //     System.out.printf("\n%d! = ", c);
+        //     for(int i=c; i>=1; i--){
+        //         fatorial *= i;
+        //     }
+        //     System.out.println(fatorial);
+        // }
 
+        // 6
+        // int tam = 20;
+        // int v[] = new int[tam];
+        // int c=0;        
+        // v[0] = 0;
+        // v[1] = 1;        
+        // do{
+        //     v[c+2] = v[c+1] + v[c];
+        // }while(v[c++]<100);
+        // // exibir:
+        // for(int i=0; i<c; i++){
+        //     System.out.printf("%d ", v[i]);
+        // }
+
+        // 7
+        // int x=13;        
+        // while(x!=1){
+        //     if(x%2==0){
+        //         x /=2;
+        //     }else{
+        //         x = 3*x + 1;
+        //     } 
+        //     System.out.printf("%d ", x);    
+        // }
     }
 }

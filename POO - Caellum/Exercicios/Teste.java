462fd1ba43743f9080fee5c1ee0bdb9d488ad7db
myller-silva <myllersilva1310@gmail.com>
2021-10-11 11:18:21 -0300

    update

--- POO - Caellum/Exercicios/Teste.java
+++ POO - Caellum/Exercicios/Teste.java
@@ -1,3 +1,11 @@
 public class Teste {
-    
+    public static void main(String[] args){
+        int d=4;
+        if((d=5)==5){
+            System.out.println(d);
+            System.out.println("ok");
+        }
+        System.out.println(d);
+
+    }    
 }

d8e6b9de4e3a119431f97d5e06cdf05d6df866fc
myller-silva <myllersilva1310@gmail.com>
2021-10-11 20:46:02 -0300

    update

--- POO - Caellum/Testes/Programa.java
+++ POO - Caellum/Testes/Programa.java
@@ -3,16 +3,18 @@ public class Programa {
         Conta minhaconta = new Conta();
         minhaconta.titular = "mac myller da silva carlos";
         minhaconta.numero = 12021999;
-        minhaconta.saldo = 422.28;  
-        System.out.println("Saldo anterior ao saque: R$" + minhaconta.saldo);     
+        minhaconta.saldo = 422.28;    
+
         if( !minhaconta.sacar(2000) ){
             System.out.println("Erro ao sacar");
         }
-        System.out.println("Saldo posterior ao saque: R$" + minhaconta.saldo);  
-        minhaconta.depositar(1073.8);
-        System.out.println("Saldo posterior ao deposito: R$" + minhaconta.saldo);
-
-
-    }
-    
+        minhaconta.depositar(1073.8);        
+        System.out.println("Saldo: R$" + minhaconta.saldo);
+        
+        Conta c2 = minhaconta;
+        c2.sacar(1000);
+        System.out.println("\n\nSaldo da minha conta:\tR$" + minhaconta.saldo);
+        System.out.println("Saldo da conta c2:\tR$" + c2.saldo);
+        
+    }    
 }

b97a43a9d35f712e66cccbc80f238f13f1c09172
myller-silva <myllersilva1310@gmail.com>
2021-10-11 11:04:57 -0300

    update

---
+++ POO - Caellum/Exercicios/Cap3_3.java
@@ -0,0 +1,13 @@
+public class Cap3_3{
+    public static void main(String[] args){
+        // trimestre
+        double janeiro = 15000;
+        double fevereiro = 23000;
+        double marco = 17000;
+        double despesa_total = janeiro+fevereiro+marco;
+        double media_mensal_de_gastos = despesa_total/3;
+
+        System.out.printf("Despesa total: %.2f\nMedia mensal de gastos: %.2f\n", despesa_total, media_mensal_de_gastos);
+        
+    }
+}

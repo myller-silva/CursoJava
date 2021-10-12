a7685346e987ec0dcc69b8b2c9ceb99d1933b610
myller-silva <myllersilva1310@gmail.com>
2021-10-11 22:21:35 -0300

    update

--- POO - Caellum/Testes/Conta.java
+++ POO - Caellum/Testes/Conta.java
@@ -1,19 +1,46 @@
 public class Conta{
     int numero;
-    String titular;
+    Cliente titular;
     double saldo;
 
-    boolean sacar(double valor_a_sacar){
-        if(valor_a_sacar<=this.saldo){            
-            double novo_saldo = this.saldo - valor_a_sacar;  
+    boolean sacar(double valor){
+        if(valor<=this.saldo){            
+            double novo_saldo = this.saldo - valor;  
             this.saldo = novo_saldo;
             return true;
         }
         return false;
     }
 
-    void depositar(double valor_a_depositar){
-        this.saldo += valor_a_depositar; 
+    void depositar(double valor ){
+        this.saldo += valor; 
     }
 
+    boolean transferir_para( Conta destino, double valor ){
+        // if(valor<=this.saldo){
+        //     destino.saldo += valor;
+        //     this.saldo -= valor;
+        // }
+        if( !this.sacar(valor) ){
+            return false;
+        }else{
+            destino.depositar(valor);
+            return true;
+        }
+    }
+    void exibir_dados(){
+        System.out.printf("Numero: %d\n", this.numero);
+        System.out.printf("Titular: %s\n", this.titular.nome);
+        System.out.printf("Saldo: R$%.2f\n", this.saldo);
+                
+    }
+
+
+
+}
+
+class Cliente{
+    String nome;
+    String sobrenome;
+    String cpf;
 }
--- POO - Caellum/Testes/Programa.java
+++ POO - Caellum/Testes/Programa.java
@@ -1,20 +1,31 @@
 public class Programa {
     public static void main(String[] args){
         Conta minhaconta = new Conta();
-        minhaconta.titular = "mac myller da silva carlos";
+        // minhaconta.titular = "mac myller da silva carlos";
+        Cliente cliente1 = new Cliente();
+        cliente1.cpf = "07409841362";
+        cliente1.nome = "mac myller";
+        cliente1.sobrenome = "da silva carlos";
+        minhaconta.titular = cliente1;
         minhaconta.numero = 12021999;
-        minhaconta.saldo = 422.28;    
+        minhaconta.saldo = 422.28;   
+        minhaconta.depositar(1073.8);   
 
-        if( !minhaconta.sacar(2000) ){
-            System.out.println("Erro ao sacar");
-        }
-        minhaconta.depositar(1073.8);        
-        System.out.println("Saldo: R$" + minhaconta.saldo);
-        
-        Conta c2 = minhaconta;
-        c2.sacar(1000);
-        System.out.println("\n\nSaldo da minha conta:\tR$" + minhaconta.saldo);
-        System.out.println("Saldo da conta c2:\tR$" + c2.saldo);
+        Conta c2 = new Conta();
+        Cliente cliente2 = new Cliente();
+        cliente2.cpf = "93874128427";
+        cliente2.nome = "maria eridan";
+        cliente2.sobrenome = "da silva";
+        c2.titular = cliente2;
+        c2.numero = 7051975;
+        c2.saldo = 500;
+
+        minhaconta.transferir_para(c2, 1000);
+        minhaconta.exibir_dados();
+        System.out.println();
+        c2.exibir_dados();
         
     }    
+
+    
 }

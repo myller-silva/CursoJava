784831e596464c87ec25906434fddc2c4eca01d8
myller-silva <myllersilva1310@gmail.com>
2021-11-23 16:23:10 -0300

    update

--- - POO - Faculdade/Excecoes/Atividades/Atividade1/questao5/Cadastro.java
+++ - POO - Faculdade/Excecoes/Atividades/Atividade1/questao5/Cadastro.java
@@ -1,29 +1,48 @@
 import java.util.Scanner;
-
 public class Cadastro {
     public static void main(String[] args) {
-        // nomeCompleto e telefone
+
         Scanner scan = new Scanner( System.in );
-        String nome;
-        int ddd, numero;
+        String nome=null;
+        int ddd, numero; 
         
-        System.out.println("Informe seu nome completo: ");
-        nome = scan.nextLine();
+        while( nome==null ){
+            System.out.println("Informe seu nome completo: ");
+            nome = scan.nextLine();
+        }
         
-        System.out.println("Informe seu numero ");
+        System.out.println("Informe seu numero ");   
+        while(true){
+            try{           
+                System.out.print("DDD: ");  
+                ddd = scan.nextInt();
+                if(100<=ddd){
+                    throw new java.util.InputMismatchException("DDD com mais de dois digitos");
+                }
+                break;
+            }catch(java.util.InputMismatchException e){
+                System.out.println("Erro, DDD invalido, tente novamente");
+                scan.nextLine(); //esvaziar buffer  
+            }
+        }
 
-        System.out.print("DDD: ");        
-        ddd = scan.nextInt();
-
-        System.out.print("Numero: ");
-        numero = scan.nextInt();
+        while(true){
+            try{           
+                System.out.print("Numero: ");  
+                numero = scan.nextInt();
+                if( numero < 900000000 || 1000000000 <= numero   ){
+                    throw new java.util.InputMismatchException("Numero invalido"); 
+                }
+                break;
+            }catch(java.util.InputMismatchException e){
+                System.out.println("Erro, Numero invalido, tente novamente");
+                scan.nextLine(); //esvaziar buffer  
+            }
+        }
 
         scan.close();
 
-
-
-
-
-        
+        System.out.println("\n\n\nNome: " + nome);
+        System.out.printf("Numero: %d %d\n", ddd, numero);
     }    
 }

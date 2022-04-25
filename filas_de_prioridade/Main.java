e4b3c11c37d076e14891bfc87c2ffd37254d09b8
Mac Myller <myllersilva1310@gmail.com>
2022-04-25 16:55:04 +0000

    update

---
+++ filas_de_prioridade/Main.java
@@ -0,0 +1,21 @@
+public class Main{
+	
+	public static void main(String args[]){
+		System.out.println("");	
+	}
+	
+}
+
+
+class ListaCircular{
+	
+}
+
+
+class Node{
+	int id; 
+	int tempo; //[1, 10]
+	int tamanho; //[20, 200]
+	
+	Node next;
+}

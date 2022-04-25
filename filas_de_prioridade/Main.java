574fbd70c87b0ca352bf13e142a986b644b63665
Mac Myller <myllersilva1310@gmail.com>
2022-04-25 17:17:22 +0000

    update

--- filas_de_prioridade/Main.java
+++ filas_de_prioridade/Main.java
@@ -1,21 +1,26 @@
 public class Main{
-	
 	public static void main(String args[]){
 		System.out.println("");	
-	}
+	}	
+}
+
+class ListaCircular{ //duas filas de prioridade
 	
 }
 
-
-class ListaCircular{
-	
+class ListaMemoriaLivre{ //organizar do menor espaço para o maior
+	NodeMemoria inicio;
+	int length;
 }
-
 
 class Node{
 	int id; 
-	int tempo; //[1, 10]
-	int tamanho; //[20, 200]
-	
+	int tempo; // [1, 10]
+	int tamanho; //[20, 200]	
 	Node next;
+}
+
+class NodeMemoria{
+	int inicio; //0
+	int fim; //10k
 }

87c425c665f67159c12e07bcce467f8ec7ab8fba
Myller Silva <myllersilva1310@gmail.com>
2022-04-27 16:11:50 -0300

    Update Main.java

--- estrutura_de_dados/testes/Main.java
+++ estrutura_de_dados/testes/Main.java
@@ -6,19 +6,15 @@ public class Main {
 
     ListaCircular listaProgramas = new ListaCircular();
 
-    listaProgramas.add(4, 500, true);
-    // programas.add(3, 550, false);
-    // programas.add(5, 400, false);
-    // programas.add(1, 600, true);
-    // programas.add(2, 250, true);
-    // programas.add(6, 398, false);
-    // programas.add(8, 120, false);
-    // programas.add(7, 600, true);
-    // programas.add(9, 100, true);
+    listaProgramas.add(2, 500, true);
+    listaProgramas.add(1, 550, false);
+    listaProgramas.add(3, 398, false);
 
+    
     System.out.println(listaProgramas);
-    System.out.println("\n"+listaProgramas.length()+"\n");
-    // programas.print();
+    System.out.println();
+    listaProgramas.print();
+    System.out.println();
   }
 }
 
@@ -36,8 +32,8 @@ class Node{
   public Node(int id, int tempo, int tamanho, boolean maisMemoria){
     this();
     this.id = id;
-    this.tempo = tempo;
-    this.tamanho = tamanho;
+    this.tempo = ( (tempo<0) ? (tempo*(-1)) : tempo );
+    this.tamanho = ( (tamanho<0) ? (tamanho*(-1)) : tamanho );
     this.maisMemoria = maisMemoria;
   }
   
@@ -50,70 +46,98 @@ class Node{
 
 
 class ListaCircular {
-  Node p;
-  Node u;
+  Node ultimo;
   private int contadorProgramas=0;
   private int length=0;
 
   public ListaCircular() {
-    this.p = this.u = null;
+    this.ultimo = null;
   }
   public boolean isEmpty() {
-    return (this.p==null && this.u==null);
+    return ( this.ultimo==null && length==0 );
+    // return ( this.ultimo==null );
+    // return ( length==0 );
   }
-  // public void executarProgramas() {}
 
   public int length() {
     return length;
   }
+  
+  public Node remove(Node nodeAnterior){
+    if(nodeAnterior==null) return null;
+    if(isEmpty()) return null;
+    if(length==1){
+      Node temp = nodeAnterior;
+      nodeAnterior=null;
+      ultimo=null;
+      length--;
+      return temp;
+    }
+    if(length==2){
+      Node temp = nodeAnterior;
+      ultimo=nodeAnterior.next;
+      ultimo.next=ultimo;
+      length--;
+      return temp;
+    }
+    
+    
+    Node no = nodeAnterior.next;
+    if(no.id==ultimo.id){
+      ultimo = nodeAnterior;
+    }
+    nodeAnterior.next = no.next;
+    length--;
+    return no;
+   }
+
+   
 
   public Node add(int tempo, int tamanho, boolean maisMemoria) {
+    if(tempo<=0 || tamanho<=0) return null;
+
     Node no = new Node(contadorProgramas++, tempo, tamanho, maisMemoria);
-    
     if(isEmpty()){
-      this.p = this.u = no;
-      this.p.next = this.p;
+      this.ultimo = no;
+      this.ultimo.next = no;
       length++;
       return no;
     }
-    no.next = this.p;
-    this.u.next = no;
-    this.u = no;
+    Node primeiro = this.ultimo.next;
+    no.next = primeiro;
+    ultimo.next=no;
+    ultimo=no;
     length++;
     return no;
   }
 
   public void print() {  
-    int ciclo=0;
     if(isEmpty()) return;
-    Node no = this.p;
+    if(ultimo==null) return;
+    Node anterior = this.ultimo;
+    Node no = this.ultimo.next;
     do{
-      if(no.id == this.p.id) System.out.println("\n"+ciclo++);
-
-      System.out.println(no);  
-      if (no.tempo==0){
-        // pop(no)
-
-        break;
+      System.out.println(no);
+      if (--no.tempo<=0) {
+        Node removido = remove(anterior); 
+        System.out.println(">>> removido: "+removido);
       }
-      no.tempo--;
+      anterior=anterior.next;
       no=no.next;
-      try { 
-        Thread.sleep (1000); 
-      } catch (InterruptedException ex) {}
-    }while (true);
+      // try { Thread.sleep (1000); } catch (InterruptedException ex) {}
+    }while(!isEmpty());
   }
   
   @Override
   public String toString() {
     if(isEmpty()) return "[]";
     String str="";
-    Node no = this.p;
+    Node primeiro = this.ultimo.next;
+    Node no = primeiro;
     do{
-      str+=no+"\n";   
-      no=no.next; 
-    }while (no.id != this.p.id);
-
+      str+=no+"\n";
+      no=no.next;
+    }while (no.id != primeiro.id);
     return str;
   }
 }

3a730d9e6ba82184978304009648bcf6818bc4a5
Myller Silva <myllersilva1310@gmail.com>
2022-05-03 22:11:22 -0300

    Update Main2.java

--- estrutura_de_dados/atividade/Main2.java
+++ estrutura_de_dados/atividade/Main2.java
@@ -4,9 +4,14 @@ public class Main2 {
   public static void main(String[] args) {
     utils_my.Console.limparTela();
     
-    String n1="10203";
-    String n2="4005";
+    String n1="4321";
+    String n2="321";
     String resultado = multiplicar(n1,n2);
+    
+    System.out.printf("%20s\n", n1);
+    System.out.printf("%20s\n", n2);
+    System.out.printf("%20s", "x__________\n");
+    System.out.printf("%20s\n", resultado);
   }
 
   public static String multiplicar(String str1, String str2) {
@@ -19,7 +24,6 @@ public class Main2 {
       tamMaior = tamMenor; tamMenor = temp;
     }
 
-    ListaDupla resultado = new ListaDupla();
     ListaDupla listaMaior = particionar(strMaior);
     ListaDupla listaMenor = particionar(strMenor);
 
@@ -27,30 +31,59 @@ public class Main2 {
     Node node2 = listaMaior.fim;
     
     // calcular:
+    ListaDupla resultado = new ListaDupla();
+    Node nodeAux = null;
     node1=listaMenor.fim;
 
     while (node1!=null) {
       node2=listaMaior.fim;
       int op1 = Integer.parseInt(node1.value);
+      ListaDupla listaAux = new ListaDupla();
       while (node2!=null) {
         int op2 = Integer.parseInt(node2.value);
-        buffer = operar(op1, op2, buffer, resultado);
+        buffer = operar(op1, op2, buffer, listaAux);
         node2 = node2.anterior;
         if (node2==null){
-          resultado.addStart(buffer);
+          listaAux.addStart(buffer);
           buffer=0;
         }
       }
-      System.out.println();
-      System.out.println();
+      if(nodeAux==null) {
+        resultado = copy(listaAux);
+      }else{
+        int bufferSomatorio = somatorio(listaAux, nodeAux);
+        if (bufferSomatorio!=0) resultado.addStart(bufferSomatorio);
+      }
+      nodeAux = (nodeAux==null)?resultado.fim.anterior : nodeAux.anterior;
       node1=node1.anterior;
     }
-    System.out.printf("%20s\n", listaMaior);
-    System.out.printf("%20s\n", listaMenor);
-    System.out.println();
-    System.out.printf("%20s\n", resultado);
-    System.out.println();
+    
     return resultado.str();
+  }
+
+  public static int somatorio(ListaDupla listaAux, Node nodeAux){
+    int buffer=0, res=0;
+    Node no = listaAux.fim;
+    while (nodeAux!=null) {
+      int n1 = Integer.parseInt(no.value);
+      int n2 = Integer.parseInt(nodeAux.value);
+      res = n1 + n2 + buffer;
+      buffer = res/100;
+      res -= buffer*100; 
+      nodeAux.value = ""+res;
+      no = no.anterior;
+      nodeAux = nodeAux.anterior;      
+    }
+    return buffer;
+  }
+
+  public static ListaDupla copy(ListaDupla lista) {
+    ListaDupla temp = new ListaDupla();
+    while (!lista.isEmpty()) {
+      temp.addEnd(lista.inicio.value);
+      lista.deleteStart();
+    }
+    return temp;
   }
 
   public static int operar(int n1, int n2, int buffer, ListaDupla lista) {
@@ -61,8 +94,6 @@ public class Main2 {
     lista.addStart(res);
     return buffer;
   }
-
-
 
   public static ListaDupla particionar(String str) {
     ListaDupla lista = new ListaDupla();

68266f96654a26fb1cd1fcd5b86df2566ea9547e
Myller Silva <myllersilva1310@gmail.com>
2022-05-11 09:32:31 -0300

    Update Testes.java

--- estrutura_de_dados/Testes.java
+++ estrutura_de_dados/Testes.java
@@ -1,13 +1,7 @@
-import java.text.NumberFormat;
-
 
 public class Testes {
-
-  
   public static void main(String[] args) {
     utils_my.Console.limparTela();
-    String n="1120100";  
-    System.out.println(NumberFormat.getIntegerInstance().format("12123"));
   }
 }
 

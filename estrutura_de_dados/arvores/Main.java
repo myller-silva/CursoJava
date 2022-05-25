797ab9935870652623bdd9bff8294a63a675ccdf
Myller Silva <myllersilva1310@gmail.com>
2022-05-25 12:48:33 -0300

    Update Main.java

--- estrutura_de_dados/arvores/Main.java
+++ estrutura_de_dados/arvores/Main.java
@@ -1,21 +1,19 @@
 package arvores;
 
-
 // cls; javac arvores/Main.java ; java arvores/Main
 public class Main {
   public static void main(String[] args) {
     TreeList list = new TreeList();
-    
 
-    list.insert( 10, 5, 15, 13, 20, 30, 2, 3, 25, 35 ); 
-    
+    list.insert(10, 5, 15, 13, 20, 30, 2, 3, 25, 35);
+
     // list.inOrder();
     // list.preOrder();
     // list.posOrder();
 
     System.out.println();
-    System.out.println(list.children(10, 5, 15, 13, 20, 30, 2, 3, 25, 35 )); 
+    System.out.println(list.children(10, 5, 15, 13, 20, 30, 2, 3, 25, 35));
     System.out.println(list.children(123));
-    
+
   }
 }

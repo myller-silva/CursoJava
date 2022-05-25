5aa9704e4c2096a13a1f6683b889eccab7c9b7fe
Myller Silva <myllersilva1310@gmail.com>
2022-05-25 10:16:15 -0300

    Update Main.java

--- estrutura_de_dados/arvores/Main.java
+++ estrutura_de_dados/arvores/Main.java
@@ -6,13 +6,15 @@ public class Main {
   public static void main(String[] args) {
     TreeList list = new TreeList();
     
-    list.insert( 10 );
-    list.insert( 5 );
-    list.insert( 3 );
-    list.insert( 7 );
-    list.insert( 15 );
-    list.insert( 13 ); 
+    // list.insert( 10 );
+    // list.insert( 5 );
+    // list.insert( 3 );
+    // list.insert( 7 );
+    // list.insert( 15 );
+    // list.insert( 13 ); 
 
+    list.insert( 13, 4, 2 ); 
+    
     list.inOrder();
     list.preOrder();
     list.posOrder();

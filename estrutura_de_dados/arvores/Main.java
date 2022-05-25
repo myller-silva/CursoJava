cd0a04023c6d187e9c43935ccbc43985d1ae0413
Myller Silva <myllersilva1310@gmail.com>
2022-05-25 18:21:04 -0300

    Update Main.java

--- estrutura_de_dados/arvores/Main.java
+++ estrutura_de_dados/arvores/Main.java
@@ -5,15 +5,13 @@ public class Main {
   public static void main(String[] args) {
     TreeList tree = new TreeList();
 
-    tree.insert(10, 5, 15, 13, 20, 30, 2, 3, 25, 35, 7, 18, 16);
-    System.out.println(tree.children(10, 5, 15, 13, 20, 30, 2, 3, 25, 35, 7, 18, 16));
-    
-    tree.inOrder();
-    // list.preOrder();
-    // list.posOrder();
+    int[] values = { 10, 5, 15, 13, 20, 30, 2, 3, 25, 35, 7, 18, 16 };
 
-    System.out.println();
-    System.out.println("removido: "+tree.remove(2));
+    tree.insert(values);
+
+    System.out.println(tree.children(values));
+
     tree.inOrder();
+    
   }
 }

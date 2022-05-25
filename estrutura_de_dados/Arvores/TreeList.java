f7377d46fe87726d58484bbf26af06441a5cad98
Myller Silva <myllersilva1310@gmail.com>
2022-05-25 09:26:52 -0300

    Update TreeList.java

--- estrutura_de_dados/Arvores/TreeList.java
+++ estrutura_de_dados/Arvores/TreeList.java
@@ -2,35 +2,85 @@ package arvores;
 
 public class TreeList {
   TreeNode root;
-  
-  boolean insert(TreeNode ptr, int item){
+  TreeList(){
+    root=null;
+  }
+
+  public TreeNode insert(int item){
+    return insert(this.root, item);
+  }
+
+  TreeNode insert(TreeNode ptr, int item){
     if(ptr==null){
       ptr = new TreeNode(item);
-      ptr.left = ptr.right = null;
-      this.root = ptr;
-      return true;
+      ptr.left = null;
+      ptr.right = null;
+      if(root == null) {
+        this.root = ptr;
+      }
+      return ptr;
     }else{
+      TreeNode aux = null;
       if(item < ptr.value ){
-        insert(ptr.left, item);
-        return true;
-      } else if(item > root.value ){
-        insert(ptr.right, item);
-        return true;
+        if(ptr.left == null){
+          ptr.left = insert(ptr.left, item);
+        }else{
+          aux = insert(ptr.left, item);
+        }
+        return (aux==null) ? ptr.left : aux;
+      } else if(item > ptr.value ){
+        if(ptr.right == null){
+          ptr.right = insert(ptr.right, item);
+        }else{
+          aux = insert(ptr.right, item);
+        }
+        return (aux==null) ? ptr.right : aux;
       }else{
-        return false;
+        return null;
       }
     }
   }
+  
+  // String inOrderStr(){
+  //   String str="";
+  //   return str;
+  // }
+
+  void inOrder(){
+    inOrder(this.root);
+    System.out.println();
+  }
+  void inOrder(TreeNode node){
+    if (node!=null){
+      inOrder(node.left);
+      System.out.printf("%3d ", node.value);
+      inOrder(node.right);
+    } 
+  }
+
+  void preOrder(){
+    preOrder(this.root);
+    System.out.println();
+  }
+  void preOrder(TreeNode node){
+    if (node!=null){
+      System.out.printf("%3d ", node.value);
+      inOrder(node.left);
+      inOrder(node.right);
+    } 
+  }
+
+  void posOrder(){
+    posOrder(this.root);    
+    System.out.println();
+  }
+  void posOrder(TreeNode node){
+    if (node!=null){
+      inOrder(node.left);
+      inOrder(node.right);
+      System.out.printf("%3d ", node.value);
+    } 
+  }
 
 
-
-  String inOrder(){
-    return "";
-  }
-  String preOrder(){
-    return "";
-  }
-  String posOrder(){
-    return "";
-  }
 }

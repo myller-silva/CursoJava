33ffd836668aa82a49497f1410e9fc2cc6d1810f
Myller Silva <myllersilva1310@gmail.com>
2022-05-27 19:50:31 -0300

    Create Main2.java

---
+++ outros/Arquivos/Main2.java
@@ -0,0 +1,19 @@
+public class Main{
+
+  public static void main(String[] args) throws java.io.IOException {
+    int inicio = 544;
+    int fim = 618;
+
+    for (int i = inicio; i <= fim; i++) {
+      java.io.File arq_old = new java.io.File("supreme-magus-capitulo-"+i+"-centralnovel.com.pdf");
+      java.io.File arq_new = new java.io.File(i+".pdf");
+      
+      if (arq_new.exists()) throw new java.io.IOException("arquivo "+arq_new+" existe");
+      
+      boolean success = arq_old.renameTo(arq_new);
+      if (success) System.out.println(i+" Arquivo renomeado com sucesso");
+      else System.out.println(i+" Arquivo nao renomeado");
+    }
+  }
+
+}

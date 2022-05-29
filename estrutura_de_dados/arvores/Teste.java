16bda00f443f9d27b69c964c8dc8c8e5fb9a0f7d
Myller Silva <myllersilva1310@gmail.com>
2022-05-29 12:20:20 -0300

    Create Teste.java

---
+++ estrutura_de_dados/arvores/Teste.java
@@ -0,0 +1,26 @@
+package arvores;
+
+import java.util.Scanner;
+
+public class Teste {
+  public static void main(String[] args) {
+    TreeList tree = new TreeList();
+
+    int[] values = {10, 30, 20, 15, 21, 13 };
+
+    for (int value : values) {
+      tree.insert(value);
+    }
+
+    System.out.println(tree);
+    System.out.println();
+    Scanner scan = new Scanner(System.in);
+    int n = 0 ;
+    do{
+      System.out.print("value: ");
+      n = scan.nextInt();
+      scan.nextLine();
+      // System.out.println("father de "+n+" : "+tree.getFather(n));
+    }while(true);
+  }
+}
